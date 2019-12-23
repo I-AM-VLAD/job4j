683dc5a59c34b280fe84b442e0987f51813350d1
I-AM-VLAD <vlad1slaw19982@gmail.com>
2019-12-23 23:47:14 +0200

    Простое число. [#206637]

---
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber
+{
+    public static boolean check (int num)
+    {
+        /////////////////////////////////
+        boolean prime = false;
+        /////////////////////////////////
+        if ((num % 1) == 0  && (num % num) == 0)
+            prime = true;
+        /////////////////////////////////
+        for (int i = 2; i < num; i++)
+            if (num % i == 0)
+                prime = false;
+        //////////////////////////////////
+        return prime;
+    }
+}
