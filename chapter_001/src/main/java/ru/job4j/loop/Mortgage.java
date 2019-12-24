753622e3b11dfa7abeaba41b1a0d5a2661084fbb
I-AM-VLAD <vlad1slaw19982@gmail.com>
2019-12-24 19:29:50 +0200

    Ипотека. [#206636]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(int amount, int salary, double percent) {
+        int year = 0;
+        double debt = amount;
+        //////////
+        while (debt > 0) {
+            debt = debt + (debt * (percent / 100)) - salary;
+           ++ year;
+        }
+        ////////
+        return year;
+    }
+}
