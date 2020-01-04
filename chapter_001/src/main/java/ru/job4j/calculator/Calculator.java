9767d4faaa9a2ae7df40fed526a8e9e5b44708af
I-AM-VLAD <vlad1slaw19982@gmail.com>
2020-01-04 02:40:52 +0200

    JavaDoc. Code convention [#206642]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,34 +1,53 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculator.
+ * @author Vlad Chupryna (vlad1slaw19982@gmail.com).
+ */
 public class Calculator
 {
-
-    public static void add (double first, double second)
-    {
+    /**
+     * Method add.
+     * @param first - first.
+     * @param second - second.
+     */
+    public static void add (double first, double second) {
         double result =  first + second;
         System.out.println (first + " + " + second + " = " + result);
     }
-
-    public static void div (double first, double second)
-    {
+    /**
+     * Method div.
+     * @param first - first.
+     * @param second - second.
+     */
+    public static void div (double first, double second) {
         double result =  first / second;
         System.out.println (first + " / " + second + " = " + result);
     }
-
-    public static void multiply  (double first, double second)
-    {
+    /**
+     * Method multiply.
+     * @param first - first.
+     * @param second - second.
+     */
+    public static void multiply  (double first, double second) {
         double result =  first * second;
         System.out.println (first + " * " + second + " = " + result);
     }
-
-    public static void subtract  (double first, double second)
-    {
+    /**
+     * Method subtract.
+     * @param first - first.
+     * @param second - second.
+     */
+    public static void subtract  (double first, double second) {
         double result =  first - second;
         System.out.println (first + " - " + second + " = " + result);
     }
 
-    public static void main (String [] args)
-    {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main (String [] args) {
         add (1, 1);
         div (4, 2);
         multiply (2, 1);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Vlad Chupryna (vlad1slaw19982@gmail.com)
+ * @version $Id$
+ * @since 04.01.2020
+ */
+package ru.job4j.calculator;
