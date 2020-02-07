1d15181aa1c71053ffdfa243dba850c9079ee1ee
I-AM-VLAD <vlad1slaw19982@gmail.com>
2020-02-08 00:21:36 +0200

    Перегрузка конструктора. [#206733]

--- chapter_002/src/main/java/ru/job4j/tracker/Predator.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Predator.java
@@ -1,6 +1,9 @@
 package ru.job4j.tracker;
 
 public class Predator extends Animal {
+    public Predator() {
+        super();
+    }
 
     public Predator(String varConstructor) {
         super(varConstructor);
--- chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
@@ -1,7 +1,13 @@
 package ru.job4j.tracker;
 
 public class Tiger extends Predator {
+    public Tiger() {
+        super();
+    }
+
     public Tiger(String varConstructor) {
         super(varConstructor);
     }
+
+
 }
