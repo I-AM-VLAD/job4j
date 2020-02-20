ee6097cae3947e2847e4bd85230e0fc9c9707613
I-AM-VLAD <vlad1slaw19982@gmail.com>
2020-02-20 22:58:33 +0200

    Модель данных. [#206739]

---
+++ chapter_002/src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,12 @@
+package ru.job4j.pojo;
+
+public class College {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setFullName("Vlad ");
+        student.setGroup("121-16-1 ");
+        student.setReceiptDate("01.09.16");
+        System.out.println(student.getFullName() + student.getGroup() + student.getReceiptDate());
+    }
+}
